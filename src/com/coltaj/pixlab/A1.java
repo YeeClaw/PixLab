e824fa92520a0da57a1e17b08e22c9d8b20dbbc3
YeeClaw <austin@jcolt.net>
2021-01-14 11:18:49 -0700

    Finish A1

---
+++ src/com/coltaj/pixlab/A1.java
@@ -0,0 +1,11 @@
+package com.coltaj.pixlab;
+
+public class A1 {
+
+    public A1() {
+
+        System.out.println("1) It takes 8 bits to represent values from 0 - 255\n" +
+                "2) For the whole RGB model it takes 24 bits\n" +
+                "3) There are " + 640  * 480 +" pixels in 640x480 resolution");
+    }
+}
--- src/com/coltaj/pixlab/Main.java
+++ src/com/coltaj/pixlab/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        A1 a1 = new A1();
     }
 }
