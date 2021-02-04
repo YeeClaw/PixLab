322f750e96d3bf0d01810985f2760e0e005498e4
YeeClaw <austin@jcolt.net>
2021-02-04 11:20:26 -0700

    Complete exercises 1-4

--- src/Picture.java
+++ src/Picture.java
@@ -95,6 +95,33 @@ public class Picture extends SimplePicture {
 		}
 	}
 
+	public void keepOnlyBlue() {
+
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				
+				if (pixelObj.getBlue() < 150 || pixelObj.getRed() >= 200) {
+					pixelObj.setRed(0);
+					pixelObj.setGreen(0);
+					pixelObj.setBlue(0);
+				}
+			}
+		}
+	}
+
+	public void negate() {
+
+		Pixel[][] pixels = this.getPixels2D();
+		for (Pixel[] rowArray : pixels) {
+			for (Pixel pixelObj : rowArray) {
+				pixelObj.setBlue(0);
+				pixelObj.setGreen(0);
+				pixelObj.setRed(0);
+			}
+		}
+	}
+
 	/**
 	 * Method that mirrors the picture around a vertical mirror in the center of
 	 * the picture from left to right
--- src/PictureTester.java
+++ src/PictureTester.java
@@ -17,6 +17,21 @@ public class PictureTester
     beach.explore();
   }
   
+  public static void testKeepOnlyBlue() {
+
+    Picture beach = new Picture("catcry.png");
+    beach.explore();
+    beach.keepOnlyBlue();
+    beach.explore();
+  }
+
+  public static void testNegate() {
+    Picture beach = new Picture("beach.jpg");
+    beach.explore();
+    beach.negate();
+    beach.explore();
+  }
+
   /** Method to test mirrorVertical */
   public static void testMirrorVertical()
   {
@@ -58,11 +73,11 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
-    testZeroBlue();
+    //testZeroBlue();
     //testKeepOnlyBlue();
     //testKeepOnlyRed();
     //testKeepOnlyGreen();
-    //testNegate();
+    testNegate();
     //testGrayscale();
     //testFixUnderwater();
     //testMirrorVertical();
